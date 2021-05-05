560452ac2288b9590ecb6b476f8781d25bf0be42
Saulo <sauloveigr@hotmail.com>
2021-05-05 20:02:50 -0300

    Create button-arrow hover

--- js/button-scroll.js
+++ js/button-scroll.js
@@ -3,7 +3,6 @@ const buttonScroll = document.querySelector('#button-scroll');
 buttonScroll.addEventListener('click', function () {
     window.scrollTo ({
         top: 0,
-        left: 0,
         behavior: "smooth"
     });
 });
